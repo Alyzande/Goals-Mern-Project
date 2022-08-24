7415b9a4e34b67be3c07e13f48782af2744a71be
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-24 15:43:18 +0100

    first error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -11,7 +11,11 @@ const getGoals = (req, res) => {
 // @access  Private
 
 const setGoal = (req, res) => {
-    console.log(req.body)
+   if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+   }
+    
     res.status(200).json({ "message": "set goal" })
     
 }
