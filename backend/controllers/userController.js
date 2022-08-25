9639e0200679354a8c4ddb1b9ab2ccda72a57afa
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-25 10:49:30 +0100

    tidying, salting pw

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken1');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const asyncHandler =  require('express-async-handler');
 const User = require('../models/userModel');
@@ -6,23 +6,52 @@ const User = require('../models/userModel');
 // @desc    create new user
 // @route   POST /api/users
 // @access  public
-const registerUser = async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    
+    if (!name || !email || !password) {
+      res.status(400) //bad request
+      throw new Error('Please add all fields')
+    }
+
+    //check if user exists
+    const userExists = await User.findOne({email})
+
+    //if user email (unique!) already there, then user exists already
+    if(userExists) {
+      res.status(400)
+      throw new Error('User already exists')
+    }
+
+    //hash pw
+    //generate salt, 10 rounds default
+    const salt = await bcrypt.genSalt(10)
+    //takes plain pw and salt
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
     res.json({ "message": "Register User"})
-};
+});
 
 // @desc    login/authenticate user
 // @route   POST /api/users/login
 // @access  public
-const loginUser = async (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({ "message": "Authenticate User"})
-};
+});
 
 // @desc    get user info from db
 // @route   GET /api/users/me
 // @access  public
-const getMe = async (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({ "message": "user info from db here"})
-};
+});
 
 
 module.exports = {
