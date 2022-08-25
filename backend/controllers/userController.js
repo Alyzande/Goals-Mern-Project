ac2aa824b99574a265ed990fd3dae4eec8454238
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-25 10:10:13 +0100

    user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,24 @@
+// @desc    create new user
+// @route   POST /api/users
+// @access  public
 const registerUser = (req, res) => {
     res.json({ "message": "Register User"})
 }
+
+// @desc    login/authenticate user
+// @route   POST /api/users/login
+// @access  public
+const loginUser = (req, res) => {
+    res.json({ "message": "Authenticate User"})
+}
+
+// @desc    get user info from db
+// @route   GET /api/users/me
+// @access  public
+const getMe = (req, res) => {
+    res.json({ "message": "user info from db here"})
+}
+
 
 module.exports = {
     registerUser
