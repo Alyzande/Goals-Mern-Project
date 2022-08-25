ba76886815663f8a35770ea9f4b33215506e61a1
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-25 10:34:59 +0100

    install bcrypt and jwt. Asynchandler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,30 @@
+const jwt = require('jsonwebtoken1');
+const bcrypt = require('bcryptjs');
+const asyncHandler =  require('express-async-handler');
+const User = require('../models/userModel');
+
 // @desc    create new user
 // @route   POST /api/users
 // @access  public
-const registerUser = (req, res) => {
+const registerUser = async (req, res) => {
     res.json({ "message": "Register User"})
-}
+};
 
 // @desc    login/authenticate user
 // @route   POST /api/users/login
 // @access  public
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
     res.json({ "message": "Authenticate User"})
-}
+};
 
 // @desc    get user info from db
 // @route   GET /api/users/me
 // @access  public
-const getMe = (req, res) => {
+const getMe = async (req, res) => {
     res.json({ "message": "user info from db here"})
-}
+};
 
 
 module.exports = {
     registerUser, loginUser, getMe
-}
+};
