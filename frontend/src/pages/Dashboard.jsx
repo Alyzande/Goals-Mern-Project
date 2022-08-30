098cf2ee30e0071364c789b8aa04756e3dfcb2c3
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-30 13:56:34 +0100

    not logged in, redirect to login

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Spinner from '../components/Spinner';
+
+
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const {user} = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    //not logged in, redirect to /login page
+    if(!user) {
+      navigate('/login')
+    }
+
+  }, [user, navigate])
+
+
+
   return (
     <div>Dashboard</div>
   )
