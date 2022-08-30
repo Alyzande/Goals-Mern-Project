4e57abc7931ab24355eee94ef64da8f668e5140f
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-30 17:34:18 +0100

    Goal Items

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -16,48 +16,46 @@ function Dashboard() {
   const {goals, isLoading, isError, message} = useSelector((state) => state.goals);
 
   useEffect(() => {
-    //error
     if (isError) {
       console.log(message)
     }
 
-    //not logged in, redirect to /login page
-    if(!user) {
+    if (!user) {
       navigate('/login')
     }
+
     dispatch(getGoals())
 
-    //return from the use effect
     return () => {
       dispatch(reset())
     }
-
   }, [user, navigate, isError, message, dispatch])
 
-if (isLoading) {
-  return <Spinner />
-}
+  if (isLoading) {
+    return <Spinner />
+  }
 
-  return ( <>
-  <section className='heading'>
-    <h1>Welcome {user && user.name}</h1>
-    <p>Dashboard</p>
-  </section>
-  <GoalForm />
-  <section className='content'>
-    <div className="goals">
-      {goals.length > 0 ? (
-        <div className="goals">
-          {goals.map((goal) => (
-            <GoalItem />
-          ))}
-        </div>
-      ) : (<h3>You have not set any goals </h3>)}
-    </div>
-  </section>
+  return (
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
 
-  </>
+      <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
+    </>
   )
 }
-
 export default Dashboard
