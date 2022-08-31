5941e1722764ab0db6850700a443c96eb68a50a7
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-31 10:15:35 +0100

    goalservice

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,61 +1,33 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import GoalForm from '../components/GoalForm'
-import GoalItem from '../components/GoalItem'
-import Spinner from '../components/Spinner'
-import { getGoals, reset } from '../features/goals/goalSlice'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Spinner from '../components/Spinner';
+
+
 
 function Dashboard() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
 
-  const { user } = useSelector((state) => state.auth)
-  const { goals, isLoading, isError, message } = useSelector(
-    (state) => state.goals
-  )
+  const {user} = useSelector((state) => state.auth);
 
   useEffect(() => {
-    if (isError) {
-      console.log(message)
-    }
-
-    if (!user) {
+    //not logged in, redirect to /login page
+    if(!user) {
       navigate('/login')
     }
 
-    dispatch(getGoals())
+  }, [user, navigate])
 
-    return () => {
-      dispatch(reset())
-    }
-  }, [user, navigate, isError, message, dispatch])
 
-  if (isLoading) {
-    return <Spinner />
-  }
 
-  return (
-    <>
-      <section className='heading'>
-        <h1>Welcome {user && user.name}</h1>
-        <p>Goals Dashboard</p>
-      </section>
+  return ( <>
+  <section className='heading'>
+    <h1>Welcome {user && user.name}</h1>
+    <p>Dashboard</p>
+  </section>
+  
 
-      <GoalForm />
-
-      <section className='content'>
-        {goals.length > 0 ? (
-          <div className='goals'>
-            {goals.map((goal) => (
-              <GoalItem key={goal._id} goal={goal} />
-            ))}
-          </div>
-        ) : (
-          <h3>You have not set any goals</h3>
-        )}
-      </section>
-    </>
+  </>
   )
 }
 
