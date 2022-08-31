2e1d97bd41718ed0865262e31b4a9bddda83bc9f
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-08-31 08:52:04 +0100

    WIP

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Spinner from '../components/Spinner';
 import GoalForm from '../components/GoalForm';
 import { getGoals, reset } from '../features/goals/goalSlice';
-//import GoalItem from '../components/GoalItem';
+import GoalItem from '../components/GoalItem';
 
 
 
@@ -16,42 +16,46 @@ function Dashboard() {
   const {goals, isLoading, isError, message} = useSelector((state) => state.goals);
 
   useEffect(() => {
-    //error
     if (isError) {
       console.log(message)
     }
 
-    //not logged in, redirect to /login page
-    if(!user) {
+    if (!user) {
       navigate('/login')
     }
+
     dispatch(getGoals())
 
-    //return from the use effect
     return () => {
       dispatch(reset())
     }
-
   }, [user, navigate, isError, message, dispatch])
 
-if (isLoading) {
-  return <Spinner />
-}
+  if (isLoading) {
+    return <Spinner />
+  }
 
-  return ( <>
-  <section className='heading'>
-    <h1>Welcome {user && user.name}</h1>
-    <p>Dashboard</p>
-  </section>
-  <GoalForm />
-  <section className='content'>
-    <div className="goals">
-      goals here
-    </div>
-  </section>
+  return (
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
 
-  </>
+      <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
+    </>
   )
 }
-
 export default Dashboard
